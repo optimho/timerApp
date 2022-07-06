3207b1c57bbbb4ef12f76a76daa63dad991b4426
optimho <michael@duplessis.nz>
2022-07-06 13:22:41 +1200

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log('Hi there')
+
+class Timer{
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+    }
+
+    start(){
+        console.log('')
+    }
+
+
+}
