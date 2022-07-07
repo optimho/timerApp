bfc537c31667ba69aaa5fc249ab2f061392ad322
optimho <michael@duplessis.nz>
2022-07-07 12:13:14 +1200

    still creating stuff

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-console.log('Hi there')
 
-class Timer{
-    constructor(durationInput, startButton, pauseButton) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+const durationInput = document.querySelector('#duration');
+const circle = document.querySelector('circle');
 
-    }
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter.toString());
 
-    start(){
-        console.log('')
-    }
+let duration;
 
+const timerInstance = new Timer (durationInput, startButton, pauseButton, {
+    onStart(totalDuration) {
+        console.log("Timer Started")
+        duration = totalDuration
+        console.log(totalDuration)
+    },
+    onTick(timeRemaining) {
+        console.log(perimeter * timeRemaining / duration - perimeter)
+        circle.setAttribute('stroke-dashoffset', 
+            perimeter * timeRemaining / duration - perimeter
+            );
+        
+    },
+    onComplete() {
+        console.log("complete")
+    },
 
-}
+});
